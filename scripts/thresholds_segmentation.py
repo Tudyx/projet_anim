e40b10e748a1bf4e3509da446e02a44c4db9c8a6
Timothée Saint-Cast <timothee.saintcast@gmail.com>
2020-02-05 19:34:00 +0100

    Segmentation du dossier classification avec la méthode otsu

--- scripts/thresholds_segmentation.py
+++ scripts/thresholds_segmentation.py
@@ -8,47 +8,55 @@ import os
 import shutil
 from skimage import img_as_float
 import matplotlib.pyplot as plt
+from skimage.filters import threshold_multiotsu
 
 src_path = '../data/database/classification/'
-src_img_name = '148495885-148495896-001.BMP'
 dest_path = '../data/database/segmented/'
-dest_img_name = 'segmented_image.BMP'
 
-# Read original image to be segmented
-image = imread(os.path.join(src_path, src_img_name))
+files = os.listdir(src_path)
 
-# Show original image
-plt.figure()
-plt.imshow(image)
+for file in files:
 
-# Create grayscale image
-gray = rgb2gray(image)
+    # Read original image to be segmented
+    image = imread(os.path.join(src_path, file))
 
-# Show grayscale image
-plt.figure()
-plt.imshow(gray)
-# imsave(os.path.join(dest_path, 'gray_image.BMP'), gray)
+    # Show original image
+#    plt.figure()
+#    plt.imshow(image)
 
-# 1-D array conversion
-gray_r = gray.reshape(gray.shape[0]*gray.shape[1])
+    # Create grayscale image
+    gray = rgb2gray(image)
 
-# Computation of threshold segmentation
-for i in range(gray_r.shape[0]):
-    if gray_r[i] > (3/2)*gray_r.mean():
-        gray_r[i] = 3
-    elif gray_r[i] > gray_r.mean():
-        gray_r[i] = 2
-    elif gray_r[i] > (1/2)*gray_r.mean():
-        gray_r[i] = 1
-    else:
-        gray_r[i] = 0
-# 2-D array conversion
-gray = gray_r.reshape(gray.shape[0], gray.shape[1])
+    thresh = threshold_multiotsu(gray, 4)
 
-# Show segmented image
-plt.figure()
-plt.imshow(gray)
-plt.show()
+    print(thresh)
+    print(gray.mean())
 
-# Save segmented image
-imsave(os.path.join(dest_path, dest_img_name), gray)
+    # Show grayscale image
+#    plt.figure()
+#    plt.imshow(gray)
+    # imsave(os.path.join(dest_path, 'gray_image.BMP'), gray)
+
+    # 1-D array conversion
+    gray_r = gray.reshape(gray.shape[0]*gray.shape[1])
+
+    # Computation of threshold segmentation
+    for i in range(gray_r.shape[0]):
+        if gray_r[i] > thresh[2]:  # (3/2)*gray_r.mean():
+            gray_r[i] = 3
+        elif gray_r[i] > thresh[1]:  # gray_r.mean():
+            gray_r[i] = 2
+        elif gray_r[i] > thresh[0]:  # (1/2)*gray_r.mean():
+            gray_r[i] = 1
+        else:
+            gray_r[i] = 0
+    # 2-D array conversion
+    gray = gray_r.reshape(gray.shape[0], gray.shape[1])
+
+    # Show segmented image
+#    plt.figure()
+#    plt.imshow(gray)
+#    plt.show()
+
+    # Save segmented image
+    imsave(os.path.join(dest_path, file), gray)
